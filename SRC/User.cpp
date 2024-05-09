8f5aa8b13bc46e9dde22ab9c6cb90a2bf050ed5a
YassinHany1 <23-101235@students.eui.edu.eg>
2024-05-09 23:01:56 +0300

    0.4.0

--- SRC/User.cpp
+++ SRC/User.cpp
@@ -9,5 +9,15 @@
 
 boolean User::authenticate()
 {
+
 	return 0;
 }
+User::User(const string& id, const string& name, const string& pass) :
+        userId(id), userName(name), password(pass) {}
+
+    const string& User::getUserId() const { return userId; }
+    const string& User::getUserName() const { return userName; }
+
+    bool User::authenticate(const string& inputPassword) const {
+        return inputPassword == password;
+    }
--- SRC/User.h
+++ SRC/User.h
@@ -7,18 +7,28 @@
 #include <iostream>
 #include <assert.h>
 
-class User
-{
+
+#ifndef STUDENT_SYSTEM_USER_H
+#define STUDENT_SYSTEM_USER_H
+
+#include <string>
+
+using namespace std;
+
+class User {
 private:
-	string userId;
-
-	string userName;
-
-	string password;
-
+    string userId;
+    string userName;
+    string password;
 
 public:
-	boolean authenticate();
+    User(const string& id, const string& name, const string& pass);
 
+    const string& getUserId() const;
+    const string& getUserName() const;
+
+    bool authenticate(const string& inputPassword) const;
 };
+
 #endif
+
