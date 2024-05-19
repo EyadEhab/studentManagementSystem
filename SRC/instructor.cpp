81da2c78a128aee4865701350a2dbc3991c7a0bc
aliamin2005 <23-101159@student.eui.edu.eg>
2024-05-19 11:28:29 +0200

    0.7.0

--- SRC/instructor.cpp
+++ SRC/instructor.cpp
@@ -12,3 +12,49 @@ instructor::~instructor()
 {
     delete ui;
 }
+
+
+void instructor::on_updateGrade_clicked()
+{
+    bool ok;
+    int grade=ui->line1->text().toInt(&ok);
+}
+void instructor::on_getmaxgrade_clicked()
+{
+    max=0;
+    for (int i=0;i<5;i++){
+        if(max>grade[i]){
+            max=grade[i];
+        }
+    }
+    qDebug<<max<<endl;
+}
+void instructor::on_getmingrade_clicked()
+{
+    min=max;
+    for (int i=0;i<5;i++){
+        if(min<grade[i]){
+            min=grade[i];
+        }
+    }
+    qDebug<<min<<endl;
+}
+void instructor::on_getAverageGrade_clicked()
+{
+    average=0;
+    for (int i=0;i<5;i++){
+        average+=grade[i];
+        }
+    average/=5;
+
+    qDebug<<average<<endl;
+}
+
+
+
+
+
+
+
+
+
--- SRC/instructor.h
+++ SRC/instructor.h
@@ -15,6 +15,19 @@ public:
     explicit instructor(QWidget *parent = nullptr);
     ~instructor();
 
+private slots:
+    void on_getmaxgrade_2_clicked();
+
+    void on_getmaxgrade_clicked();
+
+    void on_getmingrade_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_updateGrade_clicked();
+
+    void on_getAverageGrade_clicked();
+
 private:
     Ui::instructor *ui;
 };
