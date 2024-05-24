1d7d66ae4306614ce09c647bd2dacc00a60c409c
EyadEhab <ehadehab1000@gmail.com>
2024-05-24 12:30:46 +0300

    0.13.2

--- SRC/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_adminstrator.cpp
+++ SRC/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_adminstrator.cpp
@@ -7,6 +7,7 @@
 *****************************************************************************/
 
 #include "../../../adminstrator.h"
+#include <QtGui/qtextcursor.h>
 #include <QtCore/qmetatype.h>
 
 #include <QtCore/qtmochelpers.h>
